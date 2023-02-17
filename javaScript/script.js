38729f4a77ee3a1c02b0fc0216a90f766b16a7e9
Jack Kelly <Carnificis@users.noreply.github.com>
2023-02-16 20:48:16 -0600

    New HTML

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,7 +1,8 @@
 var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={534b29d66afe9258da891c84ad465ce';
 var APIKey = '534b29d66afe9258da891c84ad465ce5'
 
-document.getElementById("check-btn").addEventListener("click",function(){
+document.getElementById("check-btn").addEventListener("click",function(event){
+  event.preventDefault()
     var cityInput = document.getElementById("search-txt").value 
     console.log(cityInput)
     currentForecast(cityInput)
@@ -17,7 +18,7 @@ function currentForecast(city){
     }).then(apiData =>{
         console.log(apiData)
     
-        var htmlTEXT = `<div class="carousel-item active">
+        var htmlTEXT = `<div class="cards">
         <div class="d-flex justify-content-between w-70">
           <div>
             <h2 ><strong>${apiData.main.temp}°F</strong></h2>
@@ -49,14 +50,13 @@ function fiveDayForecast(city){
         console.log(apiData)
         var htmlTEXT =""
         for(let i=0;i<apiData.length;i=i+8){
-        htmlTEXT += `<div class="carousel-item active">
-        <div class="d-flex justify-content-between mb-4 pb-2">
+        htmlTEXT += `<div class="cards five-day">
+        <div class=" mb-4 pb-2">
           <div>
-            <h2 ><strong>${apiData[i].main.temp}°F</strong></h2>
-            <p class="text-muted mb-0">${city}</p>
-          </div>
-          <div>
-            <img src="https://openweathermap.org/img/wn/${apiData[i].weather[0].icon}d@2x.png"
+            <h5 ><strong>${apiData[i].main.temp}°F</strong></h5>
+            <p class="text-muted mb-0">${dayjs(apiData[i].dt_text).format('DD/MM/YYYY')}</p>
+        
+            <img src="https://openweathermap.org/img/wn/${apiData[i].weather[0].icon}@2x.png"
               width="150px">
               <p >Description:${apiData[i].weather[0].description}</p>
               <p >Humidity:${apiData[i].main.humidity}</p>
